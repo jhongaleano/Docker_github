cad7e9dcae4e54f093068b9f524e57ee66764b5a
jhon Galeano <jhongaleanomartin@gmail.com>
2026-08-26 20:25:09 -0500

    FIX: Solucion primer error , Debug=true

--- sample_app.py
+++ sample_app.py
@@ -99,6 +99,6 @@ def registro():
 
 if __name__ == '__main__':
     hostPort = os.getenv("HOST","0.0.0.0") # nosec B104
-    #debug = os.getenv("DEBUG","False").lower()in ("true","1")
+    debug = os.getenv("DEBUG","False").lower()in ("true","1")
     table_BD()
-    app.run(host=hostPort, port=5050, debug=True)
+    app.run(host=hostPort, port=5050, debug=debug)
